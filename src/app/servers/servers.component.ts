08ea462778abe41b73b660a7ca2e8b2eb34a2e6b
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-12 20:34:02 -0700

    Added property binding with new button

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = true;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = false;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
