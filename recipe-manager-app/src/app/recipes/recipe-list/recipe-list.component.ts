ad92453bcdb31d01088908158e1c132d6f9cd422
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-13 18:39:37 -0700

    Added test recipe and updated RecipeListComponent

--- recipe-manager-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-manager-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "app/recipes/recipe-list/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This simply a test', 'https://c1.staticflickr.com/1/108/275776506_ecfd29e6cf_b.jpg')
+    , new Recipe('A Test Recipe', 'This simply a test', 'https://c1.staticflickr.com/1/108/275776506_ecfd29e6cf_b.jpg')
+  
+  ];
 
   constructor() { }
 
