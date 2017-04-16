967f6f9aa58d9fef8d6fa7132cad530ebf3ab9e9
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-15 23:51:11 -0700

    Aside: Updated appBetterHighlighter directive to use element property binding with @HostBinding

--- directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
+++ directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,10 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string | null = null;
 
   // Using new Renderer2 class
   constructor(private element: ElementRef, 
@@ -15,11 +16,13 @@ export class BetterHighlightDirective implements OnInit {
   
   @HostListener('mouseenter')
   onMouseEnter($event: Event) {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'lightblue');  
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'lightblue');
+    this.backgroundColor = 'lightblue'; 
   }
 
   @HostListener('mouseleave')
   onMouseLeave($event: Event) {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', null);  
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', null);
+    this.backgroundColor = null;
   }
 }
