f036045bbea5e8667fa6bdc7e7fa8e04436a9509
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-16 00:19:41 -0700

    Aside: appBetterHighlight directive using a config object approach with [DIRECTIVE_NAME] syntax

--- directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
+++ directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,11 +1,17 @@
 import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
+
+// config object interface
+interface betterHighlightConfig {
+  defaultColor?: string | null,
+  highlightColor: string
+};
+
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @Input() defaultColor: null = null;
-  @Input() highlightColor: string | null = 'lightblue';
+  @Input('appBetterHighlight') config: betterHighlightConfig;
   @HostBinding('style.backgroundColor') backgroundColor: string | null;
 
   // Using new Renderer2 class
@@ -14,18 +20,18 @@ export class BetterHighlightDirective implements OnInit {
 
   ngOnInit(): void {
     // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'lightblue');
-    this.backgroundColor = this.defaultColor;
+    this.backgroundColor = this.config.defaultColor !== undefined ? this.config.defaultColor : null;
   }
   
   @HostListener('mouseenter')
   onMouseEnter($event: Event) {
     // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'lightblue');
-    this.backgroundColor = this.highlightColor; 
+    this.backgroundColor = this.config.highlightColor; 
   }
 
   @HostListener('mouseleave')
   onMouseLeave($event: Event) {
     // this.renderer.setStyle(this.element.nativeElement, 'background-color', null);
-    this.backgroundColor = this.defaultColor;
+    this.backgroundColor = this.config.defaultColor;
   }
 }
