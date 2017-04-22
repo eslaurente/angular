06f52f1621b20cd45b5791d06bcdf60ef2e91fe3
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-22 14:21:10 -0700

    Aside: Template-Driven Form: Added local reference hooks

--- forms-template-driven/src/app/app.component.ts
+++ forms-template-driven/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('aForm') aForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log('submitted: ', form);
+  // }
+
+  onSubmit() {
+    console.log(this.aForm);
   }
 }
