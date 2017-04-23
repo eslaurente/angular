98c9a6194913e604f8cc078b4669a92bb9e3aa38
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-23 00:27:31 -0700

    RecipeManager: Added template-driven form validations and form controls for ShoppingEditComponent

--- recipe-manager-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ recipe-manager-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output, Input, ViewChild, ElementRef, 
 import { Ingredient } from "app/shared/ingredient.model";
 import { ShoppingListService } from "app/services/shopping-list.service";
 import { Subscription } from "rxjs/Subscription";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,15 +11,17 @@ import { Subscription } from "rxjs/Subscription";
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
   private ingredientSelectedSubcrptn: Subscription;
-  @ViewChild('nameInput') nameInput : ElementRef;
-  @ViewChild('amountInput') amountInput: ElementRef;
+  @ViewChild('form') form: NgForm;
+
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredientSelectedSubcrptn = this.shoppingListService.ingredientSelected.subscribe((ingredient: Ingredient) => {
-      this.nameInput.nativeElement.value = ingredient.name;
-      this.amountInput.nativeElement.value = ingredient.amount;
+      this.form.setValue({
+        'name': ingredient.name,
+        'amount': ingredient.amount
+      });
     });
   }
 
@@ -26,29 +29,34 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.ingredientSelectedSubcrptn.unsubscribe();
   }
 
-  added() {
-    const nameTemp: string = this.nameInput.nativeElement.value;
-    const amount: string = this.amountInput.nativeElement.value;
-    const name: string = nameTemp.charAt(0).toUpperCase() + nameTemp.substring(1);
-    if (this.isValid(name, amount)) {
-      this.shoppingListService.addIngredient(new Ingredient(name, Number(amount)));
+  onAdded() {
+    if (this.form.control.invalid) {
+      this.form.controls['name'].markAsTouched();
+      this.form.controls['amount'].markAsTouched();
+    }
+    else {
+      const nameTemp: string = this.form.value.name;
+      const amount: string = this.form.value.amount;
+      if (this.isValid(nameTemp, amount)) {
+        const name: string = nameTemp.charAt(0).toUpperCase() + nameTemp.substring(1);
+        this.shoppingListService.addIngredient(new Ingredient(name, Number(amount)));
+      }
     }
   }
 
-  removed() {
-    const name: string = this.nameInput.nativeElement.value;
-    const amount: string = this.amountInput.nativeElement.value;
+  onRemoved() {
+    const name: string = this.form.value.name;
+    const amount: string = this.form.value.amount;
     if (this.isValid(name, amount)) {
       this.shoppingListService.removeIngredient(new Ingredient(name, Number(amount)));
     }
   }
 
-  clearForm() {
-    this.nameInput.nativeElement.value = '';
-    this.amountInput.nativeElement.value = '';
+  onCleared() {
+    this.form.reset();
   }
 
   private isValid(name: string, amount: string): boolean {
-    return name !== '' && amount !== '';
+    return (name !== '' && name !== null && name !== undefined) && (amount !== '' && amount !== null && amount !== undefined);
   }
 }
