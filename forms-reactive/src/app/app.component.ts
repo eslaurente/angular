cdea227255bb1b02848859e04a18044ceb627e4f
Emerald Laurente <e.s.laurente@gmail.com>
2017-04-22 17:13:19 -0700

    Aside: Reactive Form: Added validation error styling

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -10,6 +10,9 @@ export class AppComponent implements OnInit {
 
   genders = ['Male', 'Female'];
   signupForm: FormGroup;
+  username: FormControl;
+  email: FormControl;
+  gender: FormControl;
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
@@ -23,9 +26,16 @@ export class AppComponent implements OnInit {
         [Validators.required]
       )
     });
+    this.username = this.getControl('username');
+    this.email = this.getControl('email');
+    this.gender = this.getControl('gender');
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  private getControl(name: string): FormControl {
+    return <FormControl>this.signupForm.get(name);
   }
 }
