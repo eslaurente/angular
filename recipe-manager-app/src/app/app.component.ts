686dd2e0ee44b659155932815de95ea7d6f9b08e
Emerald Laurente <e.s.laurente@gmail.com>
2017-05-08 10:06:10 -0700

    RecipeManager: Added firebase dependency and configured app onInit() to initialize firebase

--- recipe-manager-app/src/app/app.component.ts
+++ recipe-manager-app/src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    const config = {
+      apiKey: "AIzaSyDoTWF_GHuXLTun418ajajkWWPDGI1apTc",
+      authDomain: "udemy-ng-recipe-book-a5dc3.firebaseapp.com",
+      databaseURL: "https://udemy-ng-recipe-book-a5dc3.firebaseio.com",
+      projectId: "udemy-ng-recipe-book-a5dc3",
+      storageBucket: "udemy-ng-recipe-book-a5dc3.appspot.com",
+      messagingSenderId: "924279792238"
+    };
+    firebase.initializeApp(config);
+  }
 }
