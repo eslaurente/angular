f4a695d61c2a36c5b2dec27bf4e5dd06b948da82
Emerald Laurente <e.s.laurente@gmail.com>
2017-05-08 00:13:34 -0700

    RecipeManager: Added default parameter for RecipeDetails model’s Ingredient parameter and added initialization of the RecipeDetails parameter in fetching recipes from server.

--- recipe-manager-app/src/app/services/data-storage.service.ts
+++ recipe-manager-app/src/app/services/data-storage.service.ts
@@ -6,31 +6,41 @@ import 'rxjs/Rx';
 
 import { Recipe } from 'app/shared/recipe.model';
 import { RecipeService } from "app/services/recipe.service";
+import { RecipeDetails } from "app/shared/recipe-details.model";
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http,
-                private recipeService: RecipeService) { }
+  constructor(private http: Http,
+              private recipeService: RecipeService) { }
 
-    storeRecipes(): Observable<Response> {
-        return this.http.put('https://udemy-ng-recipe-book-a5dc3.firebaseio.com/recipes.json', this.recipeService.getRecipes())
-            .catch((error: Response) => {
-                return Observable.throw(error);
-            });
-    }
+  storeRecipes(): Observable<Response> {
+    return this.http.put('https://udemy-ng-recipe-book-a5dc3.firebaseio.com/recipes.json', this.recipeService.getRecipes())
+      .catch((error: Response) => {
+        return Observable.throw(error);
+      });
+  }
 
-    fetchRecipes(): Observable<Recipe[]> {
-        return this.http.get('https://udemy-ng-recipe-book-a5dc3.firebaseio.com/recipes.json')
-            .map((response: Response) => {
-                const data: any[] =  response.json();
-                const recipeList: Recipe[] = [];
-                data.forEach((item: Recipe) => {
-                    recipeList.push(new Recipe(item.name, item.description, item.imagePath, item.details, item.id));
-                });
-                return recipeList;
-            })
-            .catch((error: any) => {
-                return Observable.throw(error);
-            });
-    }
+  fetchRecipes(): Observable<Recipe[]> {
+    return this.http.get('https://udemy-ng-recipe-book-a5dc3.firebaseio.com/recipes.json')
+      .map((response: Response) => {
+        const data: Recipe[] =  (<Recipe[]>response.json());
+        const recipeList: Recipe[] = [];
+        data.forEach((item) => {
+          let details: RecipeDetails;
+          if (!item.details) {
+            details = new RecipeDetails();
+          }
+          else {
+            details = new RecipeDetails(item.details.ingredients, item.details.instructions); 
+          }
+          recipeList.push(new Recipe(item.name, item.description, item.imagePath, details, item.id));
+        });
+        console.log(recipeList);
+        
+        return recipeList;
+      })
+      .catch((error: any) => {
+        return Observable.throw(error);
+      });
+  }
 }
--- recipe-manager-app/src/app/shared/recipe-details.model.ts
+++ recipe-manager-app/src/app/shared/recipe-details.model.ts
@@ -1,6 +1,6 @@
 import { Ingredient } from "app/shared/ingredient.model";
 
 export class RecipeDetails {
-    constructor(public ingredients: Ingredient[],
+    constructor(public ingredients: Ingredient[] = [],
                 public instructions?: string[]) {}
 }
