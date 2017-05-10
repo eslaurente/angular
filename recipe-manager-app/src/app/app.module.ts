875f71dfaba27bfdb8cfdd02b9c8da740629c29c
Emerald Laurente <e.s.laurente@gmail.com>
2017-05-10 09:03:35 -0700

    RecipeManager: Moved service module injection to CoreModule

--- recipe-manager-app/src/app/app.module.ts
+++ recipe-manager-app/src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { RecipeService } from "app/services/recipe.service";
-import { ShoppingListService } from "app/services/shopping-list.service";
 import { AppRoutesModule } from "app/app-routes/app-routes.module";
-import { DataStorageService } from "app/services/data-storage.service";
-import { AuthService } from "app/services/auth.service";
-import { AuthGuardService } from "app/services/auth-guard.service";
 import { SharedModule } from "app/shared/shared.module";
 import { AuthModule } from "app/auth/auth.module";
 import { ShoppingListModule } from "app/shopping-list/shopping-list.module";
@@ -27,13 +21,6 @@ import { CoreModule } from "app/core/core.module";
     CoreModule,
     AuthModule,
     ShoppingListModule
-  ],
-  providers: [
-    AuthService,
-    AuthGuardService,
-    DataStorageService,
-    RecipeService,
-    ShoppingListService,
   ],
   bootstrap: [ AppComponent ]
 })
--- recipe-manager-app/src/app/core/core.module.ts
+++ recipe-manager-app/src/app/core/core.module.ts
@@ -4,6 +4,11 @@ import { HeaderComponent } from "app/core/header/header.component";
 import { HomeComponent } from "app/core/home/home.component";
 import { SharedModule } from "app/shared/shared.module";
 import { AppRoutesModule } from "app/app-routes/app-routes.module";
+import { AuthService } from "app/services/auth.service";
+import { AuthGuardService } from "app/services/auth-guard.service";
+import { DataStorageService } from "app/services/data-storage.service";
+import { RecipeService } from "app/services/recipe.service";
+import { ShoppingListService } from "app/services/shopping-list.service";
 
 @NgModule({
   imports: [
@@ -18,6 +23,13 @@ import { AppRoutesModule } from "app/app-routes/app-routes.module";
   declarations: [
     HeaderComponent,
     HomeComponent
+  ],
+  providers: [
+    AuthService,
+    AuthGuardService,
+    DataStorageService,
+    RecipeService,
+    ShoppingListService,
   ]
 })
 export class CoreModule { }
