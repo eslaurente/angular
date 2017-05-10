542935a7ad3216bdea9cea7e5b442f95d1e6c045
Emerald Laurente <e.s.laurente@gmail.com>
2017-05-10 14:59:15 -0700

    RecipeManager: Added new div to test other animations

--- asides/animations-deep-dive/src/app/app.component.ts
+++ asides/animations-deep-dive/src/app/app.component.ts
@@ -13,17 +13,39 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         'background-color': 'blue',
         'transform': 'translateX(100px)'
       })),
+      transition('normal <=> highlight', animate(300))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        'transform': 'translateX(0px) scale(1)'
+      })),
+      state('highlight', style({
+        'background-color': 'blue',
+        'transform': 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        'transform': 'translateX(0px) scale(0.5)'
+      })),
       transition('normal => highlight', animate(300)),
-      transition('highlight => normal', animate(800))
+      transition('highlight => normal', animate(800)),
+      transition('shrunken <=> *', animate(1000))
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlight' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlight' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState === 'normal' ? this.wildState = 'shrunken' : this.wildState = 'normal';
   }
 
   onAdd(item: string) {
