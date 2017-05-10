d6852a99ad1b620faeb0b0425be4b8802d8e1068
Emerald Laurente <e.s.laurente@gmail.com>
2017-05-10 14:45:45 -0700

    RecipeManager: Added simple animations. NOTE: this particular example does not work on Safari

--- asides/animations-deep-dive/src/app/app.component.ts
+++ asides/animations-deep-dive/src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-
+import { trigger, state, style, transition, animate } from '@angular/animations';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        'transform': 'translateX(0px)'
+      })),
+      state('highlight', style({
+        'background-color': 'blue',
+        'transform': 'translateX(100px)'
+      })),
+      transition('normal => highlight', animate(300)),
+      transition('highlight => normal', animate(800))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlight' : this.state = 'normal';
+  }
 
   onAdd(item: string) {
     this.list.push(item);
